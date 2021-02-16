3e1d133d7537bc52e3270d0ea638befb302ab222
Maliah Ratcliff <ratcliffm@sou.edu>
2021-02-16 12:25:23 -0800

    Coordinates started

--- coordinates.h
+++ coordinates.h
@@ -1,1 +1,14 @@
+#ifndef COORDINATES_H
+#define COORDINATES_H
 
+#include <string>
+#include <vector>
+
+// any type def goes here 
+typedef std::pair<double, double> Coordinate;
+
+// declaration of functions go here 
+double Haversine(Coordinate a, Coordinate b) {return 0};
+
+
+#endif
