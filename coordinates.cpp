5c1e6e78bb978d1c3d37047a3848d54c644e1596
Maliah Ratcliff <ratcliffm@sou.edu>
2021-02-16 19:40:04 -0800

    coord functions complete

--- coordinates.cpp
+++ coordinates.cpp
@@ -1,5 +1,7 @@
 #include "coordinates.h"
 #include <cmath> 
+#include <vector> 
+#include <utility> 
 
 double Haversine(Coordinate a, Coordinate b) 
     {   double rad = 6371; 
@@ -19,4 +21,24 @@ double Haversine(Coordinate a, Coordinate b)
         return distance; 
     }
 
+Coordinate SortByDistanceTo(CoordVec v1) { 
+    Coordinate csBuilding; 
+    csBuilding.first = 42.1858587;
+    csBuilding.second = -122.6970897; 
+    // sort through vector to return smallest coord 
+    for (unsigned int i = 0; i < v1.size(); ++i) { 
+        for (unsigned int j = i + 1; j < v1.size(); ++j) { 
+            // for number in position i of vector get haversine dist 
+            double idistance = Haversine(v1[i], csBuilding);
+            double jdistance = Haversine(v1[j], csBuilding); 
+        if (jdistance  < idistance) {
+            swap(v1[i], v1[j]);
+        }
+    }
+    }
+    
+    // return coord 
+    Coordinate coordMin = v1[0]; // vector of coords at index 0 
+    return coordMin; 
 
+} 
