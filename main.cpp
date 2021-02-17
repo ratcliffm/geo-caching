93ef11247c4f4c5b296abf9279fe3fa2430dccf1
Maliah Ratcliff <ratcliffm@sou.edu>
2021-02-16 19:38:56 -0800

    main done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include <iostream> 
+#include "coordinates.h"
+#include <vector> 
 
+using namespace std;
+
+void main() {
+    // loop that reads comma-delimited lines from standard input one line at a time and constructs a vector of coordinates 
+string coords;
+CoordVec x; 
+// Using unix pipe to get coords for input 
+while (getline(cin, coords)){
+        Coordinate userInput;
+        userInput.first = coords.at(0); 
+        userInput.second = coords.at(1); 
+            x.push_back(userInput); 
+        
+}
+
+// Sort vector of coordinates using SortByDistanceTo function
+Coordinate minCoord = SortByDistanceTo(x);
+// Print out the coordinate that is closest to the SOU Computer Science building setprecision to 6).
+
+cout << "The the coordinate pair closest to the SOU Computer Science Building is: " << minCoord.first << minCoord.second << endl; 
+}
