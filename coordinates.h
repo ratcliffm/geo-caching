9364add844849bac1580fb82363358b7515131ba
Maliah Ratcliff <ratcliffm@sou.edu>
2021-02-16 19:39:39 -0800

    declarations complete

--- coordinates.h
+++ coordinates.h
@@ -7,8 +7,14 @@
 // any type def goes here 
 typedef std::pair<double, double> Coordinate;
 
+// vector definition 
+typedef std::vector<Coordinate> CoordVec; 
+// could also use typedef std::vector<std::pair<double,double>> coordVec; but is much longer 
+
 // declaration of functions go here 
-double Haversine(Coordinate a, Coordinate b) {return 0};
+double Haversine(Coordinate a, Coordinate b);
+
+Coordinate SortByDistanceTo(CoordVec v1); 
 
 
 #endif
