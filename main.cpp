7ec344b07bb036d1c381e1880990e5bd1ebbcbe2
Maliah Ratcliff <ratcliffm@sou.edu>
2021-03-04 12:33:33 -0800

    main formatted

--- main.cpp
+++ main.cpp
@@ -11,19 +11,20 @@ int main() {
 string coords;
 CoordVec x; 
 Coordinate userInput;
+
 // Using unix pipe to get coords for input  
 while (getline(cin, coords)){
         // get whole line
         int commaLocation = coords.find(",");
         string lat = coords.substr(0, commaLocation -1); 
         string longitude = coords.substr(commaLocation + 1, coords.back());
+        // cout << lat << longitude << endl; in correct order 
         // push variable into a vector 
         userInput.first = atof(lat.c_str());
         userInput.second = atof(longitude.c_str()); 
             x.push_back(userInput); 
         
 }
-
 // Sort vector of coordinates using SortByDistanceTo function
 Coordinate minCoord = SortByDistanceTo(x);
 // Print out the coordinate that is closest to the SOU Computer Science building setprecision to 6).
