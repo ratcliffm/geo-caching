d32ccc13619fabd5548f8e138d3edd1de03aaa68
Maliah Ratcliff <ratcliffm@sou.edu>
2021-03-04 12:34:01 -0800

    coordinates formatted

--- coordinates.cpp
+++ coordinates.cpp
@@ -1,26 +1,37 @@
 #include "coordinates.h"
 #include <cmath> 
+#include <cstdlib>
 #include <iostream>
-#include <vector> 
+#include <math.h>
 #include <utility> 
 
 using namespace std;
 
-double Haversine(Coordinate a, Coordinate b) 
-    {   double rad = 6371; 
+// earth radius 
+double rad = 6371.0; 
+
+// convert decimal degrees to rads
+double degreeToRad(double deg) {
+  return (deg * M_PI / 180);
+}
+
+// convert rads to decimal degrees
+double radToDegree(double rad) {
+  return (rad * 180 / M_PI); }
+
+double Haversine(Coordinate a, Coordinate b) {
+        double lat1r, lon1r, lat2r, lon2r, latFinal, lonFinal;
+        lat1r = degreeToRad(a.first);
+        lon1r = degreeToRad(a.second);
+        lat2r = degreeToRad(b.first);
+        lon2r = degreeToRad(b.second);
         // find the distance between lat and long 
-        double latDistance = (b.first - a.first) * M_PI / 180.0; 
-        double longDistance = (b.second - a.second) * M_PI / 180.0;
+        latFinal = sin((lat2r - lat1r)/2);
+        lonFinal = sin((lon2r - lon1r)/2);
+        double distance = 2.0 * rad * asin(sqrt(latFinal * latFinal + cos(lat1r) * cos(lat2r) * lonFinal * lonFinal));
+        // to mi 
+        distance = distance / 1.609344;
 
-        // convert the distance to radians 
-        b.first = (b.first) * M_PI / 180.0; 
-        a.first = (a.first) * M_PI / 180.0; 
-
-        // find distance 
-        double powFormula; 
-        powFormula = pow(sin(latDistance / 2), 2) + pow(sin(longDistance /2), 2) * cos(b.first) * cos(a.first);
-        double c = 2 * asin(sqrt(powFormula));
-        double distance = rad * c; 
         return distance; 
     }
 
@@ -32,8 +43,8 @@ Coordinate SortByDistanceTo(CoordVec v1) {
     for (unsigned int i = 0; i < v1.size(); ++i) { 
         for (unsigned int j = i + 1; j < v1.size(); ++j) { 
             // for number in position i of vector get haversine dist 
-            double idistance = Haversine(v1[i], csBuilding);
-            double jdistance = Haversine(v1[j], csBuilding); 
+            double idistance = Haversine(csBuilding, v1[i]);
+            double jdistance = Haversine(csBuilding, v1[j]); 
         if (jdistance < idistance) {
             swap(v1[i], v1[j]);
         }
